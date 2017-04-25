a0e52bee4ba71e09c2b6aa9bd07b1f2f0c042e0c
travi <travi@DESKTOP-CVK3FRF>
2017-04-25 13:37:07 -0400

    created batch process header

---
+++ PharmacySystem/BatchProcesses.h
@@ -0,0 +1,34 @@
+#pragma once
+string ZeroFillNumber(string str, int digits);
+
+string SpaceFillString(string str, int size);
+
+string RemoveSpaces(string str);
+
+vector<string> getRecords(ofstream &batchLog, int sequenceNo, string fileName);
+
+void writeFile(vector<string> records, int sequenceNo, string fileName);
+
+void WriteBatchReviewFile(DatabaseManager *dbm, int seqNo);
+
+void WriteCreateStoreItemsFile(DatabaseManager *dbm, int seqNo);
+
+int incSeqNo(int s);
+
+vector<string> SortRecords(vector<string> records);
+
+///BATCH FUNCTIONS
+void updateItemData(DatabaseManager *dbm, ofstream &batchLog, int &sequenceNo);
+
+void createDeleteStore(DatabaseManager *dbm, ofstream &batchLog, int &sequenceNoStoreUpdate, int &sequenceNoCreateStoreItems, int &sequenceNoDeleteStoreItems);
+
+void inventoryReceivedAtWarehouse(DatabaseManager *dbm, ofstream &batchLog, int sequenceNoDeleteStoreItems, int &sequenceNoItemReceived, int &sequenceNoWarehouseInventoryUpdate);
+
+void inventoryToStoreRequest(DatabaseManager *dbm, ofstream &batchLog, int &sequenceNoStoreUpdate, int sequenceNoCreateStoreItems, int &sequenceOnlineReq, int &sequenceNoBatchRev, int &sequenceNoReorder);
+
+void inventoryGeneration(DatabaseManager *dbm, ofstream &batchLog, int &sequenceNo);
+
+void yearlySales(DatabaseManager *dbm, ofstream &batchLog, int &sequenceNo);
+
+///CALLS BATCH FUNCTIONS
+void runBatchSequence(DatabaseManager *dbm);
