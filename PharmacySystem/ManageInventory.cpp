c562401edb394d3490c8dffe783dbae7cdb1d926
Steven Serette <sserette@gmail.com>
2017-04-26 23:01:53 -0400

    added command to add inventory

--- PharmacySystem/ManageInventory.cpp
+++ PharmacySystem/ManageInventory.cpp
@@ -5,3 +5,35 @@
 #include "DatabaseManager.h"
 #include "Inventory.cpp"
 using namespace std;
+
+class ManageInventory {
+public:
+
+	void promptForInventoryInput(int itemId, int storeId) {
+		getInventoryAddInput(itemId, storeId);
+	}
+
+private:
+
+	void getInventoryAddInput(int itemId, int storeId) {
+		string quantityString;
+		int quantity;
+		
+		quantityString = getInput("Quantity");
+		try {
+			quantity = stoi(quantityString);
+		}
+		catch (const char *e) {
+			throw e;
+		}
+
+		AddItem *addItem = DatabaseManager::shared()->createAddItemOrder(itemId, storeId, quantity);
+
+		if (addItem) {
+			cout << "Item added to inventory queue successfully." << endl;
+		}
+		else {
+			throw "Unable to add item to queue.";
+		}
+	}
+};
